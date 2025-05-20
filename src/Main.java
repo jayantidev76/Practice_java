ca9cb891e44bd7a320644018a817545a56f7d40f
jayantidev76 <shawjiya64@gmail.com>
2025-05-20 16:33:13 +0530

    added three numbers

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // add two numbers
+        int x = 5;
+        int y = 7;
+        int z = 1 ;
+        int sum = x + y+ z;
+        System.out.println(sum);
     }
 }
