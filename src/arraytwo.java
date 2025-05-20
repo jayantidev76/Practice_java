17ffc87d1e6e3d7c7cfaeb690080065e277c106a
jayantidev76 <shawjiya64@gmail.com>
2025-05-20 17:54:31 +0530

    second array

---
+++ src/arraytwo.java
@@ -0,0 +1,8 @@
+public class arraytwo {
+    public static void main(String[] args) {
+        String myStr = "hello";
+        char[] myArray = myStr.toCharArray();
+        System.out.println(myArray[0]);
+
+    }
+}
