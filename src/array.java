22fc935d230da728d936c83bcf09fef37c310771
jayantidev76 <shawjiya64@gmail.com>
2025-05-20 17:48:06 +0530

    arrays

---
+++ src/array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int[] myArray = {1, 5, 10, 25};
+        int sum = 0;
+        int i;
+        for (i = 0 ; i < myArray.length; i++){
+            sum += myArray[i];
+        }
+        System.out.println("The sum is: " + sum);
+
+    }
+}
